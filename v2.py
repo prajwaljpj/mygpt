2cabe7d2f3167dc542a6e9f69ff3e6505d8ddfc4
Prajwal Rao <prajwaljpj@gmail.com>
2024-10-02 17:26:55 -0400

    adding multi headed attention

--- v2.py
+++ v2.py
@@ -98,13 +98,27 @@ class Head(nn.Module):
         return out
 
 
+# Muti headed attention
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat(
+            [h(x) for h in self.heads], dim=-1
+        )  # concatenate over the channel dimension
+
+
 # Our simple bigram model
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.sa_head = Head(n_embed)
+        self.sa_head = MultiHeadAttention(
+            4, n_embed // 4
+        )  # 4 heads of 8 dimensional self-attention # similar to grouped convolution
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
