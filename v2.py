4c6bf65a3bf8a477da67ee754fa9b432b4eb8fac
Prajwal Rao <prajwaljpj@gmail.com>
2024-10-02 18:24:13 -0400

    added dropouts and final results(readme)

--- v2.py
+++ v2.py
@@ -6,14 +6,17 @@ from torch.nn import functional as F, init
 
 # initial parameters
 torch.manual_seed(1337)
-batch_size = 32
-block_size = 8
+batch_size = 64
+block_size = 256
 max_iters = 5000
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 eval_iters = 200
-n_embed = 32
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 # Get data
 with open("input.txt", "r", encoding="utf-8") as f:
@@ -81,6 +84,7 @@ class Head(nn.Module):
         self.value = nn.Linear(n_embed, head_size, bias=False)
         # registering buffer does not consider this function as a model parameter
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
@@ -93,6 +97,7 @@ class Head(nn.Module):
             self.tril[:T, :T] == 0, float("-inf")
         )  # (B, T, T) # tril doesnt communicate with the past as for a decoder module
         wei = F.softmax(wei, dim=-1)  # (B, T, T)
+        wei = self.dropout(wei)
         v = self.value(x)
         out = wei @ v
         return out
@@ -104,12 +109,13 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(n_embed, n_embed)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat(
             [h(x) for h in self.heads], dim=-1
         )  # concatenate over the channel dimension
-        out = self.proj(out)
+        out = self.dropout(self.proj(out))
         return out
 
 
@@ -125,6 +131,7 @@ class FeedForward(nn.Module):
             nn.Linear(
                 4 * n_embed, n_embed
             ),  # inner layer is multiplied by 4 in the position-wise feedforward network
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
@@ -156,12 +163,8 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.blocks = nn.Sequential(
-            Block(n_embed, n_head=4),
-            Block(n_embed, n_head=4),
-            Block(n_embed, n_head=4),
-            nn.LayerNorm(n_embed),
-        )
+        self.blocks = nn.Sequential(*[Block(n_embed, n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embed)  # final layer norm
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -172,6 +175,7 @@ class BigramLanguageModel(nn.Module):
         )  # (T, C)
         x = tok_emb + pos_emb  # (B, T, C) because broadcasting happens to the pos_emb
         x = self.blocks(x)  # apply all the decoder blocks
+        x = self.ln_f(x)
         logits = self.lm_head(x)  # (Batch, Time, Channel)
 
         if targets is None:
@@ -207,7 +211,7 @@ m = model.to(device)
 
 optimizer = torch.optim.AdamW(m.parameters(), learning_rate)
 
-# train and validatae model
+# train and validate model
 for iter in range(max_iters):
     if iter % eval_interval == 0:
         losses = estimate_loss()
